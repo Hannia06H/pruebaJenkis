2ea092fd18f683ead7a0d5b08d61b15b8cd0bd38
Hannia06H <164127459+Hannia06H@users.noreply.github.com>
2024-10-07 00:42:40 -0600

    Sexto commit

--- test/calculator-tests.js
+++ test/calculator-tests.js
@@ -5,7 +5,7 @@ var calculator = require('../calculator');
 describe('prueba unitaria: ', function(){
     describe('vericando funcion addTest', function(){
         it('verificar valor retornado: assert.equal(val, val): ', function(){
-            resutl = calculator.addTest(1,3);
+            resutl = calculator.addTest(1,1);
             assert.equal(resutl, 2);
         });
     });
