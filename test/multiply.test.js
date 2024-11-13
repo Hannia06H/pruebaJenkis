f1b3f684460c6c216330c8d1583e7c98101329a8
Hannia06H <164127459+Hannia06H@users.noreply.github.com>
2024-11-12 23:49:51 -0600

    Create multiply.test.js

---
+++ test/multiply.test.js
@@ -0,0 +1,21 @@
+// multiply.test.js
+const assert = require('chai').assert;
+const multiply = require('./multiply');
+
+describe('Test de la función multiply', function() {
+  it('Debería multiplicar 2 y 3 para obtener 6', function() {
+    const result = multiply(2, 3);
+    assert.equal(result, 6);
+  });
+
+  it('Debería multiplicar 0 y 5 para obtener 0', function() {
+    const result = multiply(0, 5);
+    assert.equal(result, 0);
+  });
+
+  it('Debería multiplicar -2 y 3 para obtener -6', function() {
+    const result = multiply(-2, 3);
+    assert.equal(result, -6);
+  });
+});
+
